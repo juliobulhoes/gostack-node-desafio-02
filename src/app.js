26ce63066f655c9643c576322f9710412edbd99b
juliobulhoes <juliobulhoes@yahoo.com.br>
2020-08-18 21:44:18 -0300

    [upload] gostack challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -11,23 +11,83 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryId = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryId < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  const repository = {
+    ...repositories[repositoryId],
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryId] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryId = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryId < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositoryId, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryId = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryId < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  const repository = {
+    ...repositories[repositoryId],
+    likes: repositories[repositoryId].likes + 1,
+  };
+
+  repositories[repositoryId] = repository;
+
+  return response.json({ likes: repository.likes });
 });
 
 module.exports = app;
